5f0439bc60b60994b1f50e4de836ea660b139c19
RaSantacruz <oimraf@gmail.com>
2026-01-04 00:18:50 +0100

    add prefix

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const usersRouter = require('./users');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get(process.env.prefix +'/', (req, res) => {
     res.send('Hello World');
 });
-app.use('/users', usersRouter);
+app.use(process.env.prefix +'/users', usersRouter);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
